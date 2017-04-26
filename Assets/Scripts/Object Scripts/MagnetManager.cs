b75c72bfa443d4948dda817bf99f0dedb1e0b072
BradyCalvert <phreakyphred@gmail.com>
2017-04-26 10:08:07 -0700

    master

---
+++ Assets/Scripts/Object Scripts/MagnetManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetManager : MonoBehaviour
+{
+  public float duration = 1;
+  public float bounceForce = 1;
+  private GameObject[] posObj;
+
+  void Update()
+  {
+    GameObject[] posObj = GameObject.FindGameObjectsWithTag("Positive2");
+    if (posObj != null & GameObject.FindWithTag("Negative2") != null)
+      MoveObj();
+
+  }
+  void MoveObj()
+  {for(int i=0;i<posObj.Length;i++)
+    {
+      posObj[i].GetComponent<Rigidbody>().useGravity = false;
+        posObj[i].transform.position =
+        Vector3.Lerp(posObj[i].transform.position,
+        (GameObject.FindWithTag("Negative2").transform.position), Time.deltaTime / duration); }
+  }
+}
